c4607e9b4824d4f949953128023897aa72bce2ff
dawit2123 <dawitzewdu2123@gmail.com>
2024-04-06 16:45:15 +0300

    backend updated

--- server.js
+++ server.js
@@ -17,7 +17,14 @@ const app = express();
 //serving static files
 app.use(express.static(__dirname + "/public"));
 
-app.use(cors());
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5000",
+      "https://addis-music-back-end-test-project.onrender.com/",
+    ],
+  })
+);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
